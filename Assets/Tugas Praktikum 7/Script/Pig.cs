18ffe9a11d9696412b565e00cef4274d8dae7250
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2022-07-02 12:47:29 +0700

    game

---
+++ Assets/Tugas Praktikum 7/Script/Pig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Pig : MonoBehaviour
+{
+    Bandit bt;
+    // Use this for initialization
+    void Start()
+    {
+        //Warrior adl nama dari game object yg ada di hierarchy
+        bt = GameObject.Find("Warrior").GetComponent<Bandit>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            bt.nyawa--;
+        }
+        if (bt.nyawa < 0)
+        {
+            bt.play_again = true;
+        }
+    }
+}
